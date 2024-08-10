f8a8c86962ed3684f50742a16568b7942900d471
ngbcoder94 <ngbcoder94@gmail.com>
2024-08-10 13:14:39 -0400

    Class added

---
+++ codeBase/MergeTwoLinkedLists.java
@@ -0,0 +1,13 @@
+/*
+ * Welcome to my Java Class!!! 
+ * Author: Nick Burkett
+ * Created for: Leetcode problem
+ * Date: 08/09/2024
+ */
+
+public class MergeTwoLinkedLists{
+
+        public static void main(String args[]){
+                System.out.println("Hello World... my class is now set up to solve my Leetcode problem");
+        }
+}
